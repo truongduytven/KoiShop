99035afa45e0b2cdd25b6ff95a16f6ef22fc69e3
Võ Nhật Trường Duy K17 <duyvntse170126@fpt.edu.vn>
2024-10-15 21:18:28 +0700

    feat: bottom navigation

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -6,7 +6,7 @@ const HomeScreen = () => {
   return (
     <View className="flex-1 justify-center items-center bg-white">
       <Text className="text-black text-2xl font-bold">Home Screen</Text>
-      <Button mode="contained" className="mt-5" onPress={() => console.log("Pressed")}>
+      <Button mode="contained" className="mt-5 bg-secondary" onPress={() => console.log("Pressed")}>
         Go Somewhere
       </Button>
     </View>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 // tailwind.config.js
 
 module.exports = {
-   content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
-    theme: {
-      extend: {},
+  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#470101',
+        secondary: '#6d1a1a',
+        tertiari: '#faeaa3'
+      },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+}
